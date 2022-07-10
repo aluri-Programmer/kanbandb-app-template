d21abd03d02925a9f028ab0d04973d51c23fa402
sai dheeraj <saluri0328@gmail.com>
2022-07-10 09:46:23 -0400

    cleanup changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.scss';
 import Kanban from './components/Kanban/Kanban';
 import KanbanDB from 'kanbandb';
-let instance;
 
-async function initialize() {
-  /**
-   * Use KanbanDB like so (but you might want to move it) - types are provided
-   * by jsconfig.json, which will utilize d.ts files and give you autocompletion for
-   * KanbanDB, in Visual Studio Code, if that is your preferred IDE.
-   * 
-   * This code (initialize function) is for demonstration only.
-   */
-  instance = await KanbanDB.connect(null);
-  // instance.addCard({
-  //     name: 'testin',
-  //     description: 'testin',
-  //     status: 'TODO'
-  // })
-  return instance
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      kanbandb: KanbanDB.connect(), // Initialize DB communications.
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Kanban Board
+        </h1>
+        <Kanban instance={this.state.kanbandb} />
+      </div>
+    )
+  }
 }
-
-function App() {
-  const a = initialize();
-
-  return (
-    <div className="App">
-      <h1>
-        Kanban Board
-      </h1>
-      <Kanban instance={a} />
-    </div>
-  );
-}
-
-export default App;
-
